33e78952f3e0bdc8f4ff9bbafa377a4b105c9bc0
Goio <goio.monti@gmail.com>
2018-12-12 08:26:01 +0100

    sadfa

---
+++ app/src/main/java/com/example/dm2/laudio/Conver_inicial.java
@@ -0,0 +1,17 @@
+package com.example.dm2.laudio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Conver_inicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conver_inicial);
+
+
+
+
+    }
+}
